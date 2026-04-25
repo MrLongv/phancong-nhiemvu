014b4ba1f6f2b9b532333020591f85e6a9be520a
Longvan <longvanasb@gmail.com>
2026-04-25 10:20:48 +0700

    Update index.js

--- functions/api/logout/index.js
+++ functions/api/logout/index.js
@@ -1,4 +1,4 @@
-import { json } from '../_utils.js';
+import { json } from '../../_utils.js';
 export async function onRequestPost() {
   return json({ ok: true }, 200, { 'Set-Cookie': 'pc_user=; Path=/; HttpOnly; SameSite=Lax; Max-Age=0' });
 }
